3ae08f22c52a41cf385b38f6a120decc651603fa
hao <842658636@qq.com>
2020-07-29 08:32:34 +0800

    把静态文件分类

---
+++ src/main/java/com/zr0726/news/ResourceConfiguration.java
@@ -0,0 +1,16 @@
+package com.zr0726.news;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ResourceConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //addResourceHandler中的是访问路径，可以修改为其他的字符串
+        //addResourceLocations中的是实际路径
+        registry.addResourceHandler("/model/**").addResourceLocations("classpath:/model/");
+        super.addResourceHandlers(registry);
+    }
+}
